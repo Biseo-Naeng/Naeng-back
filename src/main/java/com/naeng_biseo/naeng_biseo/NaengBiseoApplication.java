d18bc36864a18a7e37c8a88e0410e3d275d69e06
cux-maks <volcano020716@gmail.com>
2024-12-11 22:13:31 +0900

    init: init project

---
+++ src/main/java/com/naeng_biseo/naeng_biseo/NaengBiseoApplication.java
@@ -0,0 +1,13 @@
+package com.naeng_biseo.naeng_biseo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaengBiseoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaengBiseoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naeng_biseo/naeng_biseo/NaengBiseoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naeng_biseo.naeng_biseo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaengBiseoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
