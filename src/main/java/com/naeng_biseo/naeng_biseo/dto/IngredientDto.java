d180c63819456be0ec183f4aee82e05aa68452e5
HOHOJIN <96985407+hohojinjin@users.noreply.github.com>
2025-01-22 21:53:03 +0900

    Update IngredientDto.java

--- src/main/java/com/naeng_biseo/naeng_biseo/dto/IngredientDto.java
+++ src/main/java/com/naeng_biseo/naeng_biseo/dto/IngredientDto.java
@@ -1,3 +1,11 @@
+package com.naeng_biseo.naeng_biseo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
