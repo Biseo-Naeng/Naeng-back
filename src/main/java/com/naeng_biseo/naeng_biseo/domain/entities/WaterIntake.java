45ffb8e60480566739c8e147a5f9e78e4b0b886a
cux-maks <volcano020716@gmail.com>
2025-02-20 19:07:55 +0900

    feat(domain): WaterIntake 테이블 정의

---
+++ src/main/java/com/naeng_biseo/naeng_biseo/domain/entities/WaterIntake.java
@@ -0,0 +1,20 @@
+package com.naeng_biseo.naeng_biseo.domain.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "WaterIntake")
+public class WaterIntake {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer waterIntakeId;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    private Integer targetAmount;
+    private Integer consumedAmount;
+
+}
