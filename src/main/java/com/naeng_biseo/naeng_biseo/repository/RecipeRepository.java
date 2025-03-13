b995545c996488d4247156a69ac0cbd52303032a
HOHOJIN <96985407+hohojinjin@users.noreply.github.com>
2025-03-13 14:42:30 +0900

    Update RecipeRepository.java

--- src/main/java/com/naeng_biseo/naeng_biseo/repository/RecipeRepository.java
+++ src/main/java/com/naeng_biseo/naeng_biseo/repository/RecipeRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+public interface RecipeRepository extends JpaRepository<Recipe, Integer> {
     Page<Recipe> findAll(Pageable pageable);
     List<Recipe> findByTitleContainingOrInstructionsContaining(String title, String instructions);
 }
