0014251fd7279015c419744b614c9498b8d86bdd
JuYeong17 <dhwndud980@gmail.com>
2025-05-23 22:23:35 +0900

    fix: redis config 변경

--- src/main/java/com/naeng_biseo/naeng_biseo/config/RedisConfig.java
+++ src/main/java/com/naeng_biseo/naeng_biseo/config/RedisConfig.java
@@ -1,16 +1,25 @@
 package com.naeng_biseo.naeng_biseo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(); // 기본 localhost:6379
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
